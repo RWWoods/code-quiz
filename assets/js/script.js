12178aa73c9241f5c7ef0977b27c6edbde7fff3a
Kiggles <ridgewwoods@gmail.com>
2023-03-28 23:14:33 -0400

    have questions progressing on button click, need answers to progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,9 @@
 var startButton = document.getElementById("startButton");
 var quiz = document.getElementById("quizBox");
 var answerButtons = document.getElementById("answers");
-var questionNum = 0;
-var choiceOptions = answerButtons.children;
+var index = -1;
 var questions = document.getElementById("initialText").textContent;
-var optionsArray = [answerButtons.children];
+
 var option1 = answerButtons.children[0].textContent;
 var option2 = answerButtons.children[1].textContent;
 var option3 = answerButtons.children[2].textContent;
@@ -27,13 +26,33 @@ var question2 = "Question Two"
 var question3 = "Question Three"
 var question4 = "Question Four"
 var questionArray = [question1, question2, question3, question4]
+var answerArray = document.getElementsByClassName("list-group-item");
+questions.innerHTML = questionArray[0];
+var answer1 = document.getElementById("answer1").textContent;
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+var optionsArray = [answer1, answer2, answer3, answer4]
+var answerQ1 = ["option1", "option2", "option3", "option4"]
+var answerQ2 = ["red", "blue", "green", "grey"]
+var answerQ3 = ["dog", "cat", "pig", "cow"]
+var answerQ4 = ["Joe", "Bob", "Dave", "Bill"]
+var answerArray = [answerQ1, answerQ2, answerQ3, answerQ4]
 
+console.log(answer1);
 
 
 startButton.addEventListener("click", startQuiz);
 answerButtons.addEventListener("click", nextQuestion);
 function nextQuestion() {
+++index;
+    if (index >= questionArray.length) {
+        alert("all questions are done!");       
+    } else {
+        document.getElementById("initialText").innerHTML = questionArray[index]
 
+    }
+console.log("hello");
 
 }
 
@@ -42,5 +61,5 @@ function startQuiz() {
     event.preventDefault;
 }
     
-console.log(questions);
-
+console.log(optionsArray);
+console.log(answerArray);
